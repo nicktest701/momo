a00f5db96f8ce5508d7a8f6aee1489a4aceccfae
nicktest701 <nicktest701@gmail.com>
2023-02-04 00:53:08 +0000

    cinema model done

--- server.js
+++ server.js
@@ -57,13 +57,11 @@ app.use("/transaction", transactionRoute);
 
 
 
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-// app.get("/", (req, res) => {
-//   res.render("university");
-// });
+
 
 //error handlers
 app.use((req, res, next) => {
